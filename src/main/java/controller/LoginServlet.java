4a55de126b8ece608f1df647fc26a248df1eeb8e
hoangducviet1993 <ducviet12vl@gmail.com>
2021-12-07 01:45:09 +0700

    ......

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -40,3 +40,46 @@ public class LoginServlet extends HttpServlet {
         }
     }
 }
+//@WebServlet(name = "LoginServlet", value = "/login")
+//public class LoginServlet2 extends HttpServlet {
+//    UserService userService = new UserService();
+//
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        showForm(request, response);
+//    }
+//
+//    private void showForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        RequestDispatcher dispatcher = request.getRequestDispatcher("login/login.jsp");
+//        dispatcher.forward(request, response);
+//
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        try {
+//            verify(request, response);
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    private void verify(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+//
+//        HttpSession session = request.getSession(true);
+//        String email = request.getParameter("email");
+//        String password = request.getParameter("password");
+//        User user = userService.verifyLogin(email, password);
+//        if (user == null) {
+//            RequestDispatcher requestDispatcher = request.getRequestDispatcher("login/login.jsp");
+//            request.setAttribute("verifyResult", "Incorrect password or email. Please try again");
+//            requestDispatcher.forward(request,response);
+//        } else {
+//            session.setAttribute("currentUser", user);
+//            RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+//            requestDispatcher.forward(request,response);
+//        }
+//    }
+//}
